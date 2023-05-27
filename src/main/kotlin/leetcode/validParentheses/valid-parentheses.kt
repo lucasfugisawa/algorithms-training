6a686d822188bbefd7d9166ff62abe71a1008212
Lucas Fugisawa <lucasfugisawa@gmail.com>
2023-05-27 16:30:46 -0300

    problems solved: valid-parentheses.

---
+++ src/main/kotlin/leetcode/validParentheses/valid-parentheses.kt
@@ -0,0 +1,23 @@
+package leetcode.validParentheses
+
+class Solution {
+
+    fun isValid(s: String): Boolean {
+
+        val pairs = mapOf(
+            '(' to ')',
+            '[' to ']',
+            '{' to '}'
+        )
+        val stack = mutableListOf<Char>()
+
+        for (c in s) {
+            if (pairs.containsKey(c)) stack.add(c)
+            else {
+                val top = stack.removeAt(stack.lastIndex) // stack.removeLastOrNull() in latest Kotlin
+                if (pairs[top] != c) return false
+            }
+        }
+        return stack.isEmpty()
+    }
+}
