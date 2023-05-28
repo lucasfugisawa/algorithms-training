729eeaa8a2341291d7bc3fa74e5c58ffab4ee72e
Lucas Fugisawa <lucasfugisawa@gmail.com>
2023-05-27 22:30:51 -0300

    problems solved: palindrome-number.

---
+++ src/test/kotlin/leetcode/palindromeNumber/PalindromeNumberTests.kt
@@ -0,0 +1,49 @@
+package leetcode.palindromeNumber
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class PalindromeNumberTests {
+
+    @Test
+    fun testIsPalindrome_Case1() {
+        // Arrange
+        val input = 121
+        val expected = true
+
+        // Act
+        val solution = Solution()
+        val result = solution.isPalindrome(input)
+
+        // Assert
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun testIsPalindrome_Case2() {
+        // Arrange
+        val input = -121
+        val expected = false
+
+        // Act
+        val solution = Solution()
+        val result = solution.isPalindrome(input)
+
+        // Assert
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun testIsPalindrome_Case3() {
+        // Arrange
+        val input = 10
+        val expected = false
+
+        // Act
+        val solution = Solution()
+        val result = solution.isPalindrome(input)
+
+        // Assert
+        assertEquals(expected, result)
+    }
+}
